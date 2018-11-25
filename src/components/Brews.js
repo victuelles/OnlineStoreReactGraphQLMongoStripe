406039ac963c8ac4ea1ffd61a01b48e18fa6b5f9
Rom Vic <technoyguy@gmail.com>
2018-11-25 13:07:19 -0800

    Add remove items persistent

--- src/components/Brews.js
+++ src/components/Brews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {Box,Heading,Card,Image,Text,Button,Mask} from 'gestalt'
+import {Box,Heading,Card,Image,Text,Button,Mask,IconButton} from 'gestalt'
+import {calculatePrice,setCart,getCart} from './utils'
 import {Link} from 'react-router-dom'
 import Strapi from 'strapi-sdk-javascript/build/main'
 const apiUrl = process.env.API_URL || 'http://localhost:1337'
@@ -36,7 +37,9 @@ class Brews extends Component {
             })
             //console.log(response)
             this.setState({brews:response.data.brand.brews,
-                    brand:response.data.brand.name})
+                    brand:response.data.brand.name,
+                    cartItems:getCart()
+                })
         }catch(err){
             console.log('Error, no data fetched')
             console.error(err)
@@ -44,8 +47,33 @@ class Brews extends Component {
 
     }
 
+    addToCart=brew=>{
+        const alreadyInCart= this.state.cartItems.findIndex(item=>item._id===brew._id)
+
+        if(alreadyInCart === -1){
+            const updateItems=this.state.cartItems.concat({
+                ...brew,
+                quantity:1
+            })
+            this.setState({cartItems:updateItems},()=>setCart(updateItems))
+        } else{
+            const updateItems=[...this.state.cartItems]
+            updateItems[alreadyInCart].quantity +=1
+            this.setState({cartItems:updateItems},()=>setCart(updateItems))
+        }
+
+    }
+
+    deleteItemFromCart=itemToDeleteId=>{
+        const filteredItems=this.state.cartItems.filter(
+            item=>item._id !==itemToDeleteId
+        )
+        this.setState({cartItems:filteredItems},()=>setCart(filteredItems))
+
+    }
     render(){
         const {brand,brews,cartItems}=this.state
+      //  console.log("cartItems",cartItems)
         return(
             <Box
             marginTop={4}
@@ -99,16 +127,16 @@ class Brews extends Component {
                              justifyContent="center"
                              direction="column"
                          >
-                         <Box marginBottom={2}>
-                             <Text size="xl">{brew.name}</Text>
-                         </Box>
-                         <Text >{brew.description}</Text>
-                         <Text color="orchid" >{brew.price}</Text>
-                         <Box marginBottom={2}></Box>
-                            <Text size="xl">
-                                <Button color="blue" text="Add to Cart"/>
-                            </Text>
-                         <Box marginBottom={2}></Box>
+                            <Box marginBottom={2}>
+                                <Text size="xl">{brew.name}</Text>
+                            </Box>
+                            <Text >{brew.description}</Text>
+                            <Text color="orchid" >${brew.price}</Text>
+                            <Box marginBottom={2}>
+                                <Text size="xl">
+                                    <Button onClick={()=>this.addToCart(brew)} color="blue" text="Add to Cart"/>
+                                </Text>
+                            </Box>
                          </Box>
 
                          </Card>
@@ -121,11 +149,27 @@ class Brews extends Component {
                     <Mask shape="rounded" wash>
                         <Box display="flex" direction="column" alignItems="center" padding={2}>
                         {/* User Cart Heading */}
-                             <Heading align="center" size="md" >Your Cart</Heading>
+                             <Heading align="center" size="sm" >Your Cart</Heading>
                              <Text color="gray" italic>
                                 {cartItems.length} items selected
                              </Text>
-                            {/*  Cart Items (will add) */}
+                            {/*  Cart Items  */}
+                            {cartItems.map(item=>(
+
+                                <Box key={item._id} display="flex" alignItems="center">
+                                    <Text>
+                                        {item.name} x {item.quantity} - {(item.quantity * item.price).toFixed(2)}
+                                    </Text>
+                                    <IconButton
+                                        accessibilityLabel="Delete Item"
+                                        icon="cancel"
+                                        size="sm"
+                                        iconColor="red"
+                                        onClick={()=>this.deleteItemFromCart(item._id)}
+                                    />
+                                </Box>
+                            ))}
+
 
                             <Box display="flex" alignItems="center" justifyContent="center" direction="column">
                                 <Box margin={2}>
@@ -134,7 +178,7 @@ class Brews extends Component {
                                     )}
                                 </Box>
 
-                                <Text size="lg">Total: $3.99</Text>
+                                <Text size="lg">Total: {calculatePrice(cartItems)}</Text>
                                 <Text>
                                     <Link to="/checkout">Checkout</Link>
                                 </Text>
